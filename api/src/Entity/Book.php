0606222147ee0ffd97c2bbd023aed75332a22b19
Артём Лейбкович <a_lejbkovich@webjox.ru>
2023-08-01 15:02:15 +0300

    dev: add validation to models, and graphql

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -5,39 +5,39 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
-/** A book. */
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity]
 #[ApiResource]
 class Book
 {
-    /** The ID of this book. */
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    #[Assert\Isbn]    
     private ?int $id = null;
 
-    /** The ISBN of this book (or null if doesn't have one). */
     #[ORM\Column(nullable: true)]
+    #[Assert\NotBlank]
     public ?string $isbn = null;
 
-    /** The title of this book. */
     #[ORM\Column]
+    #[Assert\NotBlank]
     public string $title = '';
 
-    /** The description of this book. */
     #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank]
     public string $description = '';
 
-    /** The author of this book. */
     #[ORM\Column]
+    #[Assert\NotBlank]
     public string $author = '';
 
-    /** The publication date of this book. */
     #[ORM\Column]
+    #[Assert\NotNull]
     public ?\DateTimeImmutable $publicationDate = null;
 
-    /** @var Review[] Available reviews for this book. */
     #[ORM\OneToMany(targetEntity: Review::class, mappedBy:'book', cascade:['persist', 'remove'])]
     public iterable $reviews;
-
+    
     public function __construct()
     {
         $this->reviews = new ArrayCollection();
@@ -47,5 +47,6 @@ class Book
         return $this->id;
     }
 }
+
 
 ?>
