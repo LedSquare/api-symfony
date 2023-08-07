909db29bced7e8ac793898cb8dad4a74ae5f5a7c
Артём Лейбкович <a_lejbkovich@webjox.ru>
2023-08-07 11:40:46 +0300

    Entity Question changed

--- api/src/Entity/Question.php
+++ api/src/Entity/Question.php
@@ -3,10 +3,20 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Link;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ApiResource]
+#[ApiResource(
+    uriTemplate: '/answers/{id}/related_quiestions.{_format}',
+    uriVariables: [
+        'id' => new Link(
+            fromClass: Answer::class, 
+            fromProperty: 'relatedQuestions'
+        )
+    ],
+)]
 class Question
 {
     #[ORM\Id, ORM\Column, ORM\GeneratedValue]
@@ -18,7 +28,7 @@ class Question
     #[ORM\OneToOne]
     #[ORM\JoinColumn(referencedColumnName: 'id', unique: true)]
     public Answer $answer;
-    
+
     public function getId(): ?int
     {
         return $this->id;
